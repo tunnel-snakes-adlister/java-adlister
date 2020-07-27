4eba33febb987da29d2e6a8fc38c3c7713cfede2
JahzielB <jahzielb@gmail.com>
2020-07-27 11:22:25 -0500

    ads page shows unique links to pages with more info for each ad

--- src/main/java/com/codeup/adlister/controllers/IndividualAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/IndividualAdServlet.java
@@ -1,4 +1,20 @@
 package com.codeup.adlister.controllers;
 
-public class IndividualAdServlet {
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "IndividualAdServlet", urlPatterns = "/ads/individual-ad/*")
+public class IndividualAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getPathInfo().substring(1);
+        request.setAttribute("ad", DaoFactory.getAdsDao().individualAd(id));
+        request.getRequestDispatcher("/WEB-INF/ads/individual-ad.jsp").forward(request, response);
+    }
 }
