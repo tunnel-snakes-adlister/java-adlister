83e193ad14049c73d2afbc9999e5159d78a21bc4
Justin McCaleb <justinleemccaleb@gmail.com>
2020-07-27 10:11:54 -0500

    added Users interface/MySQLUsersDao isAdmin method AND doGet/doPost in AdminServlet

--- src/main/java/com/codeup/adlister/controllers/AdminServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdminServlet.java
@@ -1,4 +1,29 @@
 package com.codeup.adlister.controllers;
 
-public class AdminServlet {
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="controllers.AdminServlet", urlPatterns = "/admin")
+public class AdminServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        if (DaoFactory.getUsersDao().isAdmin(user)) {
+            response.sendRedirect("/admin.jsp");
+        }else {
+            response.sendRedirect("/index.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
 }
